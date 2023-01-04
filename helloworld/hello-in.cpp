7a42b2e700cdf12037e56b9f06c4958becb21e9f
Josh <hello@joshmleslie.com>
2023-01-03 16:39:29 -0800

    expand hello-in with recursive prompt approval

--- helloworld/hello-in.cpp
+++ helloworld/hello-in.cpp
@@ -2,12 +2,30 @@
 #include <string>
 
 using std::string;
+using std::cout;
+using std::cin;
+
+string prompt()
+{
+
+	string userInput{};
+	char decide{'n'};
+
+	cout << "Hello. Do you have a thought?\n";
+	cin >> userInput;
+	cout << "You said, " + userInput + ". Correct?\n(y/n)";
+	cin >> decide;
+	if ('n' == decide) {
+		return prompt();
+	} else {
+		cout << "Excellent choice.\n";
+		return userInput;
+	}
+}
 
 int main()
 {
-	string input{};
-	std::cout << "Hello. Do you have a thought?\n";
-	std::cin >> input;
-	std::cout << "You said, " + input + ". Correct?\n";
+	string userInput { prompt() };
+	cout << "You decided on " + userInput + ". Most excellent.";
 	return 0;
 }
